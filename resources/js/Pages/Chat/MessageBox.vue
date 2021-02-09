59d261d8d38a76df6fee3832292b98028938b1e9
Sowren Sen <sowrensen@gmail.com>
2021-02-09 22:49:38 +0600

    decorated individual messages with MessageItem component

--- resources/js/Pages/Chat/MessageBox.vue
+++ resources/js/Pages/Chat/MessageBox.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="h-96 w-full">
     <div class="h-full p-2 flex flex-col-reverse overflow-y-auto">
-      <div v-for="(message, index) in messages" :key="index">
-        {{ message.text }}
+      <div v-for="(message, index) in messages" :key="index" :class="isCurrentUserSender(message) ? 'text-right' : 'text-left'">
+        <message-item :message="message" :self="isCurrentUserSender(message)"></message-item>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import MessageItem from '@/Pages/Chat/MessageItem'
+
 export default {
+  components: { MessageItem },
   props: ['messages'],
+  computed: {
+    currentUser () {
+      return this.$page.props.user
+    }
+  },
+  methods: {
+    isCurrentUserSender (message) {
+      return this.currentUser.id === message.user_id
+    }
+  }
 }
 </script>
 
--- resources/js/Pages/Chat/MessageItem.vue
+++ resources/js/Pages/Chat/MessageItem.vue
@@ -1,8 +1,33 @@
 <template>
-  <h3>MessageItem</h3>
+  <div class="mt-7">
+    <div class="text-gray-600 text-sm font-bold" :class="self ? 'pr-2' : 'pl-2'">
+      {{ self ? 'You' : message.user.name }}
+    </div>
+    <div class="mt-1 p-2 rounded-md text-left inline-block max-w-xl shadow-md" :class="decorativeClasses">
+      {{ message.text }}
+    </div>
+    <div class="mt-2 text-gray-500 text-xs uppercase" :class="self ? 'pr-2' : 'pl-2'">
+      {{ readableDate(message.created_at) }}
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: ['message', 'self'],
+  computed: {
+    decorativeClasses () {
+      if (this.self) {
+        return 'bg-blue-100 text-blue-900'
+      }
+      return 'bg-pink-100 text-pink-900'
+    }
+  },
+  methods: {
+    readableDate (date) {
+      return (new Date(date)).toLocaleString()
+    }
+  }
+}
 </script>
 
