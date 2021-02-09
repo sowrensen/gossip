5147c8ed54b28ed6c0753ea670a35401e7623e20
Sowren Sen <sowrensen@gmail.com>
2021-02-10 00:29:52 +0600

    enabled BroadcastServiceProvider and added a channel route

--- routes/channels.php
+++ routes/channels.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('Chat.Room.{roomId}', function ($user, $roomId) {
+    if (auth()->check()) {
+        return ['id' => $user->id, 'name' => $user->name];
+    }
+});
